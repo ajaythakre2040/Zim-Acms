510e4b4f8d966a5287341355ebf9d8e3f7c3738e
[xyz123] <[xyz123@gamil.com]>
2026-07-13 11:44:12 +0530

    update code

--- server/index.ts
+++ server/index.ts
@@ -99,7 +99,7 @@ app.use((req, res, next) => {
         })
         .where(eq(cronMaster.code, MAIN_GATE_SYNC.CODE));
 
-      // await initCronSystem();
+      await initCronSystem();
          
     } catch (e) {
       log("Cron reset failed: " + e, "error");
