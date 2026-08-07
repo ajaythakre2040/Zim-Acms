f37261d49a5eedf03b81980662ea418af25e913a
[xyz123] <[xyz123@gamil.com]>
2026-08-07 18:22:56 +0530

    update verification type

--- server/cron/mainGateAuthTask.ts
+++ server/cron/mainGateAuthTask.ts
@@ -235,6 +235,7 @@ export async function runMasterAuthSync() {
                     empCode,
                     punchTime,
                     doorId: doorDetails.id,
+                    updatedLockoutFlag: updatedLockoutFlag,
                     ruleId: ruleIdToStore
                 });
             } else {
